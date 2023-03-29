00858daf9c25337669a8ac78afabac2f34d56e0e
cah81 <carles1981@gmail.com>
2023-03-29 13:40:59 +0200

    Initial commit

---
+++ src/test/java/com/gestor/tareas/GestortareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestor.tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestortareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
